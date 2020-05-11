f988a8b3a36c0842f6ea184c1081a42d99cbc8d6
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2020-05-11 10:52:45 +0200

    add pm2

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "moneyprinter",
+      cwd: "./backend",
+      script: "yarn",
+      args: "start",
+      interpreter: "none",
+      env: {
+        NODE_ENV: "production",
+      },
+      // watch: true,
+      // ignore_watch: ['node_modules']
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "104.248.141.30",
+      ref: "origin/cms",
+      repo: "https://github.com/mahapo/decentralex-bots.git",
+      path: "/var/www/decentralex-bots",
+      "post-deploy":
+        "cd backend && yarn install && yarn tsc && yarn build && pm2 reload ecosystem.config.js --env production",
+    },
+  },
+};
