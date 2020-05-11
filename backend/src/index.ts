bb34691cd81b05bd8c83e45003885b8964a80ca4
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2020-05-11 14:40:24 +0200

    add slack logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,54 +1,27 @@
-import * as program from "commander";
+import { Slack } from "./utils/Slack";
 import { TraderLeveraged } from "./runners";
 import { Bybit } from "./exchanges";
 require("dotenv").config();
 // import Trader from "./trader";
 
-const now = new Date();
-const yesterday = new Date(now.getTime() - 24 * 60 * 60 * 1e3);
-
-function toDate(val) {
-  return new Date(val * 1e3);
-}
-
-program
-  .version("1.0.0")
-  .option(
-    "-i, --interval [interval]",
-    "Interval in seconds for candlestick",
-    parseInt
-  )
-  .option("-p, --product [product]", "Product identifier", "BTC-USD")
-  .option(
-    "-s, --start [start]",
-    "Start time in unix seconds",
-    toDate,
-    yesterday
-  )
-
-  .option("-e, --end [end]", "End time in unix seconds", toDate, now)
-  .option("-t, --strategy [strategy]", "Strategy Type")
-  .option("-r, --type [type]", "Run type")
-  .option("-f, --funds [funds]", "Amount of money to use", parseInt)
-  .option("-l, --live", "Run live")
-  .parse(process.argv);
-
 const main = async function () {
   try {
+    console = new Slack({
+      token: process.env.SLACK_TOKEN,
+      channelId: process.env.SLACK_CHANNEL,
+    });
     const account = new Bybit(
       {
         apiKey: process.env.BYBITID,
         secret: process.env.BYBITSECRET,
         enableRateLimit: true,
         rate_limit: 1000,
-        // verbose: true,
       },
-      true
+      !!process.env.DEMO
     );
 
     const trader = new TraderLeveraged(account, {});
     await account.init();
-    await account.reset("BTC/USD");
     await account.startWebSocket();
     trader.start({
       symbol: "BTC/USD",
--- backend/src/utils/Slack.ts
+++ backend/src/utils/Slack.ts
@@ -1,20 +1,51 @@
 import { WebClient } from "@slack/web-api";
-
+const { Console } = require("console");
 export class Slack {
   channelId: string;
   client: WebClient;
 
   constructor({ token, channelId }) {
-    console.log(token);
+    // super()
 
     this.client = new WebClient(token);
     this.channelId = channelId;
   }
 
-  send(text) {
+  send(text, options = {}) {
     return this.client.chat.postMessage({
       channel: this.channelId,
       text,
+      ...options,
+    });
+  }
+
+  log(...text) {
+    return this.send(text.join(" "));
+  }
+
+  debug(...text) {
+    return this.send(text.join(" "));
+  }
+
+  error(...text) {
+    return this.send(text.join(" "));
+  }
+
+  info(...text) {
+    return this.send(text.join(" "));
+  }
+
+  signal(signal) {
+    return this.send("New signal", {
+      blocks: [
+        {
+          type: "section",
+          fields: Object.keys(signal).map((key) => ({
+            type: "mrkdwn",
+            text: `*${key.toUpperCase()}:*\n${signal[key]}`,
+          })),
+        },
+      ],
     });
   }
 }
