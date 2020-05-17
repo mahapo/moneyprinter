73a169265b56e148164b75fe19f6bc31ed4117dd
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2020-05-17 20:39:51 +0200

    Add test for Stategy

--- backend/test/stepper.test.ts
+++ backend/test/stepper.test.ts
@@ -1,8 +1,31 @@
 import { ZoneRecovery } from "../src/models/ZoneRecovery";
-describe("compiler: codegen", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    console.table(ZoneRecovery.calcSteps(10, 3));
 
-    // expect(2 + 2).toBe(3);
+describe("ZoneRecovery", () => {
+  //    Ratio 2
+  //   ┌─────────┬────────┬───────┬────────┬─────────────┐
+  //   │ (index) │ factor │ total │ profit │ profitTotal │
+  //   ├─────────┼────────┼───────┼────────┼─────────────┤
+  //   │    0    │   1    │   1   │   1    │      1      │
+  //   │    1    │   2    │   3   │   2    │      1      │
+  //   │    2    │   4    │   7   │   4    │      1      │
+  //   │    3    │   8    │  15   │   8    │      1      │
+  //   │    4    │   16   │  31   │   16   │      1      │
+  //   │    5    │   32   │  63   │   32   │      1      │
+  //   │    6    │   64   │  127  │   64   │      1      │
+  //   │    7    │  128   │  255  │  128   │      1      │
+  //   │    8    │  256   │  511  │  256   │      1      │
+  //   │    9    │  512   │ 1023  │  512   │      1      │
+  //   └─────────┴────────┴───────┴────────┴─────────────┘
+  test("Ratio: 2: First Step", () => {
+    expect(ZoneRecovery.calcStep(0, 2).total).toBe(1);
+    expect(ZoneRecovery.calcStep(0, 2).factor).toBe(1);
+    expect(ZoneRecovery.calcStep(0, 2).profit).toBe(1);
+    expect(ZoneRecovery.calcStep(0, 2).profitTotal).toBe(1);
+  });
+  test("Ratio: 3: First Step", () => {
+    expect(ZoneRecovery.calcStep(0, 3).total).toBe(1);
+    expect(ZoneRecovery.calcStep(0, 3).factor).toBe(1);
+    expect(ZoneRecovery.calcStep(0, 3).profit).toBe(2);
+    expect(ZoneRecovery.calcStep(0, 3).profitTotal).toBe(2);
   });
 });
---
+++ backend/test/strategy.test.ts
@@ -0,0 +1,81 @@
+import { MoneyPrinter } from "../src/strategy/MoneyPrinter";
+
+describe("MoneyPrinter via Backtester", () => {
+  const Strategy = new MoneyPrinter(null);
+  const prices = [10000, 10030, 10020];
+  const options = {
+    timestamp: 0,
+    amount: 100,
+    leverage: 100,
+    symbol: "",
+    ratio: 2,
+  };
+
+  test("Signal create orders", () => {
+    Strategy.onSignal({ price: prices[0], ...options });
+    expect(Strategy.currentOrders.length).toBe(2);
+  });
+
+  test("Other signal don't create new orders", () => {
+    Strategy.onSignal({ price: prices[0], ...options });
+    expect(Strategy.currentOrders.length).toBe(2);
+    expect(Strategy.currentOrders[0].amount).toBe(options.amount);
+    expect(Strategy.currentOrders[1].amount).toBe(options.amount);
+  });
+
+  test("After long order is filled", () => {
+    Strategy.onOrderFilled(Strategy.currentOrders[0]);
+    expect(Strategy.countFilled).toBe(1);
+    expect(Strategy.currentOrders.length).toBe(2);
+    expect(Strategy.side).toBe("buy");
+  });
+
+  test("After short order is filled", () => {
+    Strategy.onOrderFilled(Strategy.currentOrders[1]);
+    expect(Strategy.nextSide).toBe("sell");
+    expect(Strategy.countFilled).toBe(2);
+    expect(Strategy.currentOrders.length).toBe(3);
+    expect(Strategy.currentOrders[2].amount).toBe(options.amount * 2);
+  });
+});
+
+describe("MoneyPrinter via Trader", () => {
+  const Strategy = new MoneyPrinter(null);
+  const prices = [10000, 10030, 10020];
+  const options = {
+    timestamp: 0,
+    amount: 100,
+    leverage: 100,
+    symbol: "",
+    ratio: 2,
+  };
+
+  test("Signal create orders", () => {
+    Strategy.onSignal({ price: prices[0], ...options });
+    Strategy.isLive = true;
+    expect(Strategy.currentOrders.length).toBe(2);
+  });
+
+  test("Other signal don't create new orders", () => {
+    Strategy.onSignal({ price: prices[0], ...options });
+    expect(Strategy.currentOrders.length).toBe(2);
+  });
+
+  test("After long order is filled", () => {
+    Strategy.onOrderFilled(Strategy.currentOrders[0]);
+    expect(Strategy.countFilled).toBe(1);
+    expect(Strategy.currentOrders.length).toBe(3);
+    expect(Strategy.currentOrders[2].amount).toBe(
+      options.amount * 2 + Strategy.currentOrders[0].amount // 300
+    );
+  });
+
+  test("After short order is filled", () => {
+    Strategy.onOrderFilled(Strategy.currentOrders[1]);
+    expect(Strategy.countFilled).toBe(2);
+    expect(Strategy.currentOrders.length).toBe(4);
+    expect(Strategy.currentOrders[3].amount).toBe(
+      options.amount * 2 + Strategy.currentOrders[2].amount // 300
+    );
+  });
+});
