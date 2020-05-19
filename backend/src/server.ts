32750f4cf275215d0167ca033a1f504e9959191f
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2020-05-19 13:27:30 +0200

    dev script

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,13 @@
-// import * as moduleAlias from "module-alias";
-
-// moduleAlias.addAliases({
-//   src: __dirname,
-// });
-
 import * as socketio from "socket.io";
 import * as http from "http";
 import { initSocket } from "./socket";
-import * as killPort from "kill-port";
-import * as cluster from "cluster";
 import { spawn } from "./clusters";
 
 const port = 5000;
 
-if (cluster.isMaster && false) {
-  killPort(port).then(spawn);
-} else {
-  const server = http.createServer();
-  const io = initSocket(socketio(server).of("/socket"));
+const server = http.createServer();
+const io = initSocket(socketio(server).of("/socket"));
 
-  server.listen(port, () => {
-    console.log(`Listening on port ${port}.`);
-  });
-}
+server.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
