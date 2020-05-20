3642efbcb6bd383672879f11b18ccac3e31fa73a
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2020-05-20 19:12:19 +0200

    live settings

--- backend/src/trader.ts
+++ backend/src/trader.ts
@@ -61,15 +61,10 @@ const main = async function () {
     ] : [
       {
         symbol: "BTC/USD",
-        leverage: 70,
-        ratio: 4,
-        maxSteps: 5
-      },
-      {
-        symbol: "ETH/USD",
-        leverage: 50,
-        ratio: 4,
-        maxSteps: 5
+        leverage: 75,
+        ratio: 5,
+        maxSteps: 4,
+        percentOfMaxRange: 20
       }
     ]
 
