4a9b59dee70b3caa22939af76f012659a728a289
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2020-05-26 00:15:05 +0200

    Trigger by MarkPrice

--- backend/src/exchanges/Bybit.ts
+++ backend/src/exchanges/Bybit.ts
@@ -144,7 +144,7 @@ export class Bybit extends ExchangeBase {
         stop_px: order.price,
         trigger_price: basePrice,
         price: order.price,
-        trigger_by: "LastPrice",
+        trigger_by: "MarkPrice",
         // time_in_force: "FillOrKill",
         ordertype: "Conditions",
       };
