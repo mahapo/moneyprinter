67e53901d0cf96f6ccca0336d0af53bd0b1e090f
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2020-06-11 22:42:12 +0200

    refactor stepper

--- backend/src/models/ZoneRecovery.ts
+++ backend/src/models/ZoneRecovery.ts
@@ -2,34 +2,21 @@ export class ZoneRecovery {
   static calcSteps(count: number, ratio: number) {
     return [...Array(count)].map((_, i) => ZoneRecovery.calcStep(i, ratio));
   }
-  
-  // TODO: Better implementation
+
   static calcStep(index: number, ratio: number, breakevent: boolean = false) {
     // @ts-ignore
-    if(ratio < 2 || ratio > 7) return {} // Currenty not possible
-    return [...Array(index)].reduce(
-      (step, _, i) => {
-        if (i != 0) {
-          do {
-            step.factor = step.factor + 0.5;
-            //@ts-ignore
-            step.profit = parseFloat((step.factor * (ratio - 1)).toFixed(4));
-            step.profitTotal = step.profit - step.total;
-          } while (step.profitTotal < (breakevent ? 0 : ratio - 1));
-        } else {
-          step.factor += 1;
-          step.profit = step.factor * (ratio - 1);
-          step.profitTotal = step.profit - step.total;
-        }
-        step.total += step.factor;
-        return step;
-      },
-      {
-        factor: 1,
-        total: 1,
-        profit: ratio - 1,
-        profitTotal: ratio - 1,
-      }
-    );
+    let lastStep = {
+      factor: 1,
+      profit: ratio - 1,
+      total: 1,
+      profitTotal: ratio - 1,
+    };
+    return [...Array(index)].reduce((step, _, i) => {
+      step.factor = (step.profitTotal + lastStep.total) / step.profitTotal;
+      step.total = lastStep.total + step.factor;
+      step.profit = step.factor * step.profitTotal;
+      lastStep = step;
+      return step;
+    }, lastStep);
   }
 }
