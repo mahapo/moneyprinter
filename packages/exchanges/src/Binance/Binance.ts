5dc46d6c4653ed917366fbe750a5449bfe05de7a
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-02-22 17:49:27 +0100

    closePosition TP

--- packages/exchanges/src/Binance/Binance.ts
+++ packages/exchanges/src/Binance/Binance.ts
@@ -159,15 +159,16 @@ export class Binance extends ExchangeBase {
         const takeProfit = {
           ...common,
           type: 'TAKE_PROFIT_MARKET',
-          quantity: this.instance.amountToPrecision(
-            order.symbol,
-            order.amountTakeProfit
-          ),
+          // quantity: this.instance.amountToPrecision(
+          //   order.symbol,
+          //   order.amountTakeProfit
+          // ),
           stopPrice: this.instance.priceToPrecision(
             order.symbol,
             order.takeProfit
           ),
-          newClientOrderId: order.clientOrderIdTP
+          newClientOrderId: order.clientOrderIdTP,
+          closePosition: true
         }
 
         const stopLoss = {
