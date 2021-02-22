6fcec0eea154a86942f485537e69948e285f0ffc
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-02-22 18:23:56 +0100

    fix

--- packages/exchanges/src/Binance/Binance.ts
+++ packages/exchanges/src/Binance/Binance.ts
@@ -159,16 +159,16 @@ export class Binance extends ExchangeBase {
         const takeProfit = {
           ...common,
           type: 'TAKE_PROFIT_MARKET',
-          // quantity: this.instance.amountToPrecision(
-          //   order.symbol,
-          //   order.amountTakeProfit
-          // ),
+          quantity: this.instance.amountToPrecision(
+            order.symbol,
+            order.amountLoss
+          ),
           stopPrice: this.instance.priceToPrecision(
             order.symbol,
             order.takeProfit
           ),
           newClientOrderId: order.clientOrderIdTP,
-          closePosition: true
+          reduceOnly: true
         }
 
         const stopLoss = {
