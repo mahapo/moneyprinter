e53beb0342585aec541da94daba503381fcfe74c
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-02-22 15:41:48 +0100

    gap

--- packages/strategies/src/Moneyprinter/MoneyPrinter.ts
+++ packages/strategies/src/Moneyprinter/MoneyPrinter.ts
@@ -69,7 +69,7 @@ export class MoneyPrinter extends StrategyBase {
       timestamp
     }
 
-    const percent = 50
+    const percent = 60
     this.priceRange = (percent / 100 / this.options.leverage) * price
     this.priceTop = price + this.priceRange / 2
     this.priceBottom = price - this.priceRange / 2
