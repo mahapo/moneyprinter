72b8e69693097a35158e3dcf2f1efd7a1c5d70ff
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-02-26 11:46:35 +0100

    add trade script

---
+++ bot/src/trade.ts
@@ -0,0 +1,55 @@
+import { Firebase } from './models/Firebase'
+import { TraderFutures } from '@moneyprinter/runners'
+import { Binance } from '@moneyprinter/exchanges'
+;(async () => {
+  const firebase = new Firebase()
+  firebase.refAccounts.doc('real-2').onSnapshot(async doc => {
+    var { options } = doc.data()
+
+    try {
+      const isDemo = false
+
+      const account = new Binance(options, isDemo)
+      await account.init()
+      await account.startWebSocket()
+
+      const commonSettings = {
+        leverage: 75,
+        recoveryGapDynamicAdd: 0,
+        recoveryGapInitial: 10,
+        maxSteps: 5,
+        ratio: 4
+      }
+
+      const traderSettings = [
+        {
+          symbol: 'BTC/USDT',
+          limit: 250000
+        },
+        {
+          symbol: 'ETH/USDT',
+          limit: 100000
+        },
+        {
+          symbol: 'BCH/USDT',
+          limit: 10000
+        },
+        {
+          symbol: 'LTC/USDT',
+          limit: 10000
+        }
+      ]
+
+      for (let setting of traderSettings) {
+        const trader = new TraderFutures(account, {
+          ...setting,
+          ...commonSettings
+        })
+        trader.start()
+        await new Promise(resolve => setTimeout(resolve, 500))
+      }
+    } catch (error) {
+      throw new Error(error)
+    }
+  })
+})()
