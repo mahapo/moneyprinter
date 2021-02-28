e96de4297f7cd0536751e95d4d066c88b7904d04
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-02-28 07:25:50 +0100

    use traillingstop

--- packages/runners/src/TraderFutures.ts
+++ packages/runners/src/TraderFutures.ts
@@ -137,7 +137,7 @@ export class TraderFutures extends Runner {
       this.strategy.onOrderFilled(order)
 
       await this.account.deleteOpenOrders(this.options.symbol)
-      await this.account.placeTpSLTs([this.strategy.currentOrder], false)
+      await this.account.placeTpSLTs([this.strategy.currentOrder], true)
     } catch (error) {
       Logger.error(error)
       await this.reset()
@@ -153,7 +153,7 @@ export class TraderFutures extends Runner {
 
       await this.account.deleteOpenOrders(this.options.symbol)
       if (this.strategy.currentOrder) {
-        await this.account.placeTpSLTs([this.strategy.currentOrder], false)
+        await this.account.placeTpSLTs([this.strategy.currentOrder], true)
       } else {
         this.reset()
       }
