e143a20a6cab8330e7d94bb72603773c815666e4
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-02-28 23:07:14 +0100

    no trail

--- packages/runners/src/TraderFutures.ts
+++ packages/runners/src/TraderFutures.ts
@@ -157,7 +157,7 @@ export class TraderFutures extends Runner {
       this.strategy.onOrderFilled(order)
 
       await this.account.deleteOpenOrders(this.options.symbol)
-      await this.account.placeTpSLTs([this.strategy.currentOrder], true)
+      await this.account.placeTpSLTs([this.strategy.currentOrder], false)
     } catch (error) {
       Logger.error(error)
       await this.reset()
@@ -173,7 +173,7 @@ export class TraderFutures extends Runner {
 
       await this.account.deleteOpenOrders(this.options.symbol)
       if (this.strategy.currentOrder) {
-        await this.account.placeTpSLTs([this.strategy.currentOrder], true)
+        await this.account.placeTpSLTs([this.strategy.currentOrder], false)
       } else {
         this.reset()
       }
