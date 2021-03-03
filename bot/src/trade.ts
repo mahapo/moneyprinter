41d7a29ac0d11eb465405775e56abd240ee185b4
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-03-03 01:21:53 +0100

    Sentry

--- bot/src/trade.ts
+++ bot/src/trade.ts
@@ -1,6 +1,11 @@
 import { Firebase } from './models/Firebase'
 import { TraderFutures } from '@moneyprinter/runners'
 import { Binance } from '@moneyprinter/exchanges'
+import * as Sentry from '@sentry/node'
+Sentry.init({
+  dsn:
+    'https://4a350580542f46bdb422be3fe3db3901@o395422.ingest.sentry.io/5247177'
+})
 ;(async () => {
   const firebase = new Firebase()
   firebase.refAccounts.doc('real-2').onSnapshot(async doc => {
