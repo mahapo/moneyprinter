1f817f385dd6e810c154519cc0bdd73901b974ac
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-03-22 20:42:34 +0100

    sever

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -21,7 +21,13 @@ module.exports = {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // components: true,
+  components: true,
+
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false,
+  },
 
   /*
    ** Customize the progress-bar color
