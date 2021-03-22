94d764eab8909e3bcbecee8169e3f78232b053d3
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-03-22 20:50:42 +0100

    fix

--- packages/runners/src/TraderFutures.ts
+++ packages/runners/src/TraderFutures.ts
@@ -186,9 +186,9 @@ export class TraderFutures extends Runner {
 
       this.strategy.onOrderFilled(order)
 
-      // await this.account.deleteOpenOrders(this.options.symbol)
+      await this.account.deleteOpenOrders(this.options.symbol)
       await this.account.placeTpSLTs([this.strategy.currentOrder])
-      await this.account.placeSlTs(this.strategy.currentOrder)
+      // await this.account.placeSlTs(this.strategy.currentOrder)
     } catch (error) {
       Logger.error(error)
       this.reset()
