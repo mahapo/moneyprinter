a4f014a7ab13ff36b6c7fe9d6f744d3648f0c9bb
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-03-25 20:41:39 +0100

    go

--- packages/server/src/bot.ts
+++ packages/server/src/bot.ts
@@ -18,8 +18,9 @@ app.post('/webhook', async (req, reply) => {
 
 app.ready(err => {
   if (err) throw err
-  console.info('Server ready')
+  console.info(app.printRoutes())
+
   app.io.on('connect', socket => console.info('Socket connected!', socket.id))
 })
 
-app.listen(1337)
+app.listen(1337, '0.0.0.0')
