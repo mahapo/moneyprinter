10e36d256374b72a50d3f6c574c999ca7e176023
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-07-29 01:47:24 +0200

    netlify

---
+++ .netlify/scripts/deploy-dev.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+yarn install
+# yarn build:functions
+# yarn generate
+# cp .netlify/headers_dev dist/_headers
---
+++ .netlify/scripts/deploy-prod.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+yarn install
+# yarn build:functions
+# yarn generate
+# cp .netlify/headers_prod dist/_headers
+# # cp .netlify/redirects_prod dist/_redirects
