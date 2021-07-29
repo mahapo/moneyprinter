b1f6d0ad6cfa25eb172e9092e74de63d26ef7062
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-07-29 05:16:17 +0200

    wip

---
+++ .netlify/functions/webhook.ts
@@ -0,0 +1,58 @@
+import { Handler } from '@netlify/functions'
+import { Binance } from '../src/Binance'
+
+//ticker:BLZUSDT;buy:0;sell:0;buy_strong:0;sell_strong:1;interval:1
+//ticker:{{ticker}};buy:{{plot("Buy")}};sell:{{plot("Sell")}};buy_strong:{{plot("Strong Buy")}};sell_strong:{{plot("Strong Sell")}}
+
+function objectFromMessage(message) {
+  return Object.fromEntries(
+    message.split(';').map(item => {
+      let [key, value] = item.split(':')
+      if (value === '0') {
+        value = false
+      } else if (value === '1') {
+        value = true
+      }
+      return [key, value]
+    })
+  )
+}
+
+const handler: Handler = async (event, context) => {
+  const options = objectFromMessage(event.body)
+  options.symbol = options.ticker.replace('USD', '/USD')
+
+  const binance = new Binance({
+    apiKey: 'pFRG137adrt0DbZvf9whB7kXB62ceVV9xFuub6hAa7Zh0Sil1clxhRPF0zy3kUcK',
+    secret: 'BVmZcqP64sqcYMLMiZIRIlv6AsCKdCdEJBNyhCVkBCJtz5ZqmMVXGYaEVAD3qR0o',
+    enableRateLimit: true,
+    options: { defaultType: 'future', adjustForTimeDifference: true }
+    // verbose: true
+  })
+  try {
+    const balance = await binance.getCurrentBalance('USDT')
+    const price = await binance.getLastPrice(options.ticker)
+
+    let amount = (balance / 20 / price) * binance.leverage
+    amount = binance.amountToPrecision(options.symbol, amount)
+
+    if (options.buy || options.buy_strong) {
+      binance.placeOrder(options.ticker, true, amount)
+    } else if (options.sell || options.sell_strong) {
+      binance.placeOrder(options.ticker, false, amount)
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello World' })
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  }
+}
+
+export { handler }
---
+++ .netlify/src/Binance.ts
@@ -0,0 +1,57 @@
+import { binance as BinanceCCXT } from 'ccxt'
+
+export class Binance extends BinanceCCXT {
+  constructor(options = {}, private demo = false) {
+    super(options)
+    this.leverage = 25
+  }
+
+  async getCurrentBalance(coin) {
+    const { free } = await this.fetchBalance()
+    return free[coin]
+  }
+
+  async getLastPrice(symbol) {
+    let { info } = await this.fetchTicker(symbol.replace('/', ''), {})
+    return parseFloat(info.lastPrice)
+  }
+
+  async placeOrder(symbol, isBuy, amount) {
+    const { info } = await this.fetchBalance(symbol)
+    const position = info.positions.find(
+      pos => pos.symbol === symbol.replace('/', '')
+    )
+    let quantity = Math.abs(position?.positionAmt || 0) + amount
+
+    if (
+      parseFloat(position.leverage) !== this.leverage &&
+      parseFloat(position.positionAmt) === 0
+    ) {
+      await this.fapiPrivatePostLeverage({
+        symbol: symbol.replace('/', ''),
+        leverage: this.leverage
+      })
+    }
+
+    if (!isBuy) {
+      quantity = quantity + -1
+    }
+
+    if (position) {
+      if (isBuy && parseFloat(position.positionAmt) > 0) {
+        return
+      }
+      if (!isBuy && parseFloat(position.positionAmt) < 0) {
+        return
+      }
+    }
+
+    await this.fapiPrivatePostOrder({
+      symbol,
+      side: isBuy ? 'BUY' : 'SELL',
+      type: 'MARKET',
+      quantity,
+      positionSide: 'BOTH'
+    })
+  }
+}
