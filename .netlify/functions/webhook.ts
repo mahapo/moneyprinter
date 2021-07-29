402faeb7a83b7e05ff28c6f18d28f5402e937e83
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-07-29 06:35:00 +0200

    wip

--- .netlify/functions/webhook.ts
+++ .netlify/functions/webhook.ts
@@ -25,8 +25,10 @@ const handler: Handler = async (event, context) => {
       options.symbol = options.ticker.replace('USDT', '/USDT')
     } else if (options.ticker.includes('USD')) {
       options.symbol = options.ticker.replace('USD', '/USDT')
+      options.ticker = options.ticker.replace('USD', 'USDT')
     } else if (options.ticker.includes('PERP')) {
       options.symbol = options.ticker.replace('PERP', '/USDT')
+      options.ticker = options.ticker.replace('PERP', 'USDT')
     }
     const binance = new Binance({
       apiKey:
