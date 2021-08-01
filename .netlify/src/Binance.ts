51d0f8338ac30b91481935ae4b12fb694147fe54
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-08-01 08:57:57 +0200

    50

--- .netlify/src/Binance.ts
+++ .netlify/src/Binance.ts
@@ -3,7 +3,7 @@ import { binance as BinanceCCXT } from 'ccxt'
 export class Binance extends BinanceCCXT {
   constructor(options = {}, private demo = false) {
     super(options)
-    this.leverage = 30
+    this.leverage = 50
     this.risk = 5
   }
 
