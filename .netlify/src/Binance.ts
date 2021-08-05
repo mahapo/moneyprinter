a93801ccad21956c04112f9f00ea0b674d249985
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-08-05 11:52:37 +0200

    wip

--- .netlify/src/Binance.ts
+++ .netlify/src/Binance.ts
@@ -5,7 +5,7 @@ import {
 export class Binance extends BinanceCCXT {
   constructor(options = {}, private demo = false) {
     super(options)
-    this.leverage = 30
+    this.leverage = 20
     this.risk = 5
   }
 
