7027cdda5d07700615ca68e58da74c11013cd70d
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-08-15 22:39:07 +0200

    wip

--- .netlify/src/Binance.ts
+++ .netlify/src/Binance.ts
@@ -6,7 +6,7 @@ export class Binance extends BinanceCCXT {
   constructor(options = {}, private demo = false) {
     super(options)
     this.leverage = 20
-    this.risk = 10
+    this.risk = 4
   }
 
   calcTpSL(symbol, price, isBuy) {
