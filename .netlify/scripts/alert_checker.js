8f6b269299eec6ad3618dd416e7767f9ddb23dc1
Manuel Haller Polo <manuelhaller@wearewondrous.com>
2021-08-22 02:21:09 +0200

    wip

---
+++ .netlify/scripts/alert_checker.js
@@ -0,0 +1,76 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+function objectFromMessage(message) {
+  return Object.fromEntries(
+    message.split(';').map(item => {
+      let [key, value] = item.split(':')
+      if (value === '0') {
+        value = false
+      } else if (value === '1') {
+        value = true
+      }
+      return [key, value]
+    })
+  )
+}
+const markets = {}
+const statistics = {
+  percent: 0
+}
+fs.createReadStream('./TradingView_Alerts_Log_2021-08-21.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    for (const result of results.reverse()) {
+      const alert = objectFromMessage(result.Description)
+      const symbol = alert.ticker.replace('PERP', '')
+      if (!markets[symbol]) {
+        markets[symbol] = {
+          buy_strong: 0,
+          sell_strong: 0,
+          buy: 0,
+          sell: 0,
+          count: 0,
+          trades: [],
+          raw: []
+        }
+      }
+      // markets[symbol].raw.push(result)
+      let currentTrade = markets[symbol].trades.slice(-1)[0];
+      if (currentTrade && currentTrade.status === 'open') {
+        currentTrade.status = 'closed'
+        currentTrade.exit = parseFloat(alert.close)
+        currentTrade.percent = (parseFloat(alert.close) / currentTrade.entry) - 1
+        if (currentTrade.side === 'sell') {
+          currentTrade.percent = currentTrade.percent * -1
+        }
+        statistics.percent += currentTrade.percent
+      }
+
+      const all = flase
+      if (alert.buy_strong || (alert.buy && all)) {
+        markets[symbol].buy_strong++
+        markets[symbol].trades.push({
+          status: 'open',
+          side: 'buy',
+          entry: parseFloat(alert.close)
+        })
+      } else if (alert.sell_strong || (alert.sell && all)) {
+        markets[symbol].sell_strong++
+        markets[symbol].trades.push({
+          status: 'open',
+          side: 'sell',
+          entry: parseFloat(alert.close)
+        })
+      }
+      markets[symbol].count++
+    }
+    console.log(markets);
+    console.table(statistics);
+    // [
+    //   { NAME: 'Daffy Duck', AGE: '24' },
+    //   { NAME: 'Bugs Bunny', AGE: '22' }
+    // ]
+  });
---
+++ .netlify/scripts/alert_importer.js
@@ -0,0 +1,81 @@
+const puppeteer = require('puppeteer');
+const symbols = require('./symbols.json');
+
+
+const message = 'ticker:{{ticker}};interval:{{interval}};buy:{{plot("Buy")}};sell:{{plot("Sell")}};buy_strong:{{plot("Strong Buy")}};sell_strong:{{plot("Strong Sell")}};close:{{close}};timenow:{{timenow}}';
+const interval = 5;
+
+(async () => {
+  const markets = symbols
+    .sort((a, b) => b.quoteVolume - a.quoteVolume)
+    .filter(market => market.symbol.endsWith('USDT'))
+    .map(market => market.symbol)
+    .slice(0,100)
+  console.table(markets)
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    // args: ['--disable-dev-shm-usage'],
+    // slowMo: 2
+  })
+  const page = await browser.newPage();
+
+  const waitAndlick = async (selector) => {
+    await page.waitForSelector(selector)
+    await page.click(selector, {
+      delay: 200
+    })
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  await page.goto('https://www.tradingview.com/')
+  await page.waitForSelector('.js-header-user-menu-button')
+  await page.click('.js-header-user-menu-button')
+  await page.waitForSelector('[data-name="header-user-menu-sign-in"]')
+  await page.click('[data-name="header-user-menu-sign-in"]')
+  await page.waitForSelector('.tv-signin-dialog__social')
+  await page.click('.js-show-email')
+  await page.type('input[name="username"]', "mahapo1991")
+  await page.type('input[name="password"]', "wortKenn_91")
+  await page.click('button[type="submit"]')
+  await page.waitForSelector('.widgetbar-pages')
+
+  await page.goto(
+    'https://www.tradingview.com/chart/UofOIzEh/', {
+      waitUntil: 'domcontentloaded',
+      timeout: 10000
+    }
+  )
+  // await waitAndlick('[data-name="alerts"]')
+  
+  await waitAndlick(`#header-toolbar-intervals [data-value="${interval}"]`)
+
+  for (const market of markets) {
+    await waitAndlick('#header-toolbar-symbol-search')
+    await page.waitForSelector('[data-name="symbol-search-items-dialog"]')
+    await page.type('input[data-role="search"]', market + "PERP")
+    await page.keyboard.press('Enter'); // Enter Key
+    
+    await sleep(3000)
+    // await waitAndlick('.widgetbar-widget-alerts_manage [class^="title"]')
+    page.click('.widgetbar-widget-alerts_manage [data-name="set-alert-button"]', {delay: 1000})
+    await page.keyboard.down('Alt')
+    await page.keyboard.press('A')
+    await page.keyboard.up('Alt')
+    await sleep(1000)
+    await waitAndlick('.js-main-series-select-wrap')
+    await waitAndlick('.js-main-series-select-wrap [data-id="2"]')
+    await waitAndlick('.js-condition-operator-input-wrap')
+    await waitAndlick('.js-condition-operator-input-wrap [data-id="1"]')
+    await waitAndlick('.js-buttons-group-option [data-title="Once Per Bar Close"]')
+    // await page.type('input[name="alert_exp_date"]', "2021-10-20")
+    await page.type('input[name="alert-name"]', `${market} ${interval}m`)
+    await page.click('.tv-control-textarea', { clickCount: 3 })
+    await page.type('.tv-control-textarea', message)
+    await page.click('[data-name="submit"]')
+  }
+})()
